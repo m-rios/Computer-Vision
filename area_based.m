9f181834398718cfb74d271bccdbd7db77c9028e
M. Rios Munoz <s3485781@fwn-bborg-edu-74-67.fmnsedu.rug.nl>
2018-03-14 11:58:37 +0100

    Did some stuff

--- area_based.m
+++ area_based.m
@@ -1,10 +1,10 @@
-function [disp_map] = area_based(image1, image2, kernel, method)
+function [disp_map] = area_based(image1, image2, kernel, method, max_disp)
     %kernel should be odd size
 %     assert(sum(size(kernel)/2 ~= 0)==2);
     image1 = cast(image1,'double');
     image2 = cast(image2,'double');
     assert(strcmp(method,'sd') |strcmp(method,'ad') |strcmp(method,'ncc'))
-    disp_map = zeros(size(image1));
+    disp_map = zeros(size(image1,1), size(image,2));
     trim_cols = (size(kernel,2)-1)/2;
     trim_rows = (size(kernel,1)-1)/2;
     for col1 = trim_cols+1:size(image1,2)-trim_cols-1
@@ -15,24 +15,24 @@ function [disp_map] = area_based(image1, image2, kernel, method)
                 diff = -inf;
             end
             win1 = image1(row1-trim_rows:row1+trim_rows,col1-trim_cols:col1+trim_cols).*kernel;
-            for col2 = trim_cols+1:size(image1,2)-trim_cols-1
+            for col2 = max(trim_cols + 1, col1-max_disp): min(size(image1,2) - (trim_cols + 1), col1+max_disp)
                 win2 = image2(row1-trim_rows:row1+trim_rows,col2-trim_cols:col2+trim_cols).*kernel;
                 switch(method)
                     case 'sd'
                         diff_curr = sd(win1,win2);
-                        if(diff_curr < diff)diff
+                        if diff_curr < diff
                             diff = diff_curr;
                             pos_x = col2;
                         end
                     case 'ad'
                         diff_curr = ad(win1,win2);
-                        if(diff_curr < diff)
+                        if diff_curr < diff
                             diff = diff_curr;
                             pos_x = col2;
                         end
                     case 'ncc'
                         diff_curr = ncc(win1,win2);
-                        if(diff_curr > diff)
+                        if diff_curr > diff
                             diff = diff_curr;
                             pos_x = col2;
                         end
@@ -41,5 +41,6 @@ function [disp_map] = area_based(image1, image2, kernel, method)
             disp_map(row1,col1) = abs(col1 - pos_x) ;
         end
     end
+    disp_map = cast(disp_map, 'uint8');
 end
 
--- main.m
+++ main.m
@@ -48,7 +48,40 @@ disp_ratios(1) = []
 
 
 %% Exercise 2
+kernel_5 = ones(5,5);
+kernel_9 = ones(9,9);
+kernel_13 = ones(13,13);
 
-kernel = ones(7,7);
-disp = area_based(I1,I2, kernel, 'ad');
+%ad
+disp_ad_5 = area_based(I1, I5, kernel_5, 'ad', 60);
+disp_ad_9 = area_based(I1, I5, kernel_9, 'ad', 60);
+disp_ad_13 = area_based(I1, I5, kernel_13, 'ad', 60);
+
+%sd
+disp_sd_5 = area_based(I1, I5, kernel_5, 'sd', 60);
+disp_sd_9 = area_based(I1, I5, kernel_9, 'sd', 60);
+disp_sd_13 = area_based(I1, I5, kernel_13, 'sd', 60);
+
+%ncc
+disp_ncc_5 = area_based(I1, I5, kernel_5, 'ncc', 60);
+disp_ncc_9 = area_based(I1, I5, kernel_9, 'ncc', 60);
+disp_ncc_13 = area_based(I1, I5, kernel_13, 'ncc', 60);
+
+
+disp = cast(disp, 'double');
+norm_disp = (disp - min(disp(:)))/(max(disp(:) - min(disp(:))));
+
+
+
+
+
+
+truth_d = cast(truth, 'double');
+norm_truth = (truth_d - min(truth_d(:)))/(max(truth_d(:) - min(truth_d(:))));
+
+corr = normxcorr2(norm_disp, norm_truth);
+comparison = max(corr(:));
+
+%% Exercise 3
+
 
